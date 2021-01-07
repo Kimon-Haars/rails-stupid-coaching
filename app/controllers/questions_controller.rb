148919d44e86da686d3316214d0f755c2a863c39
Kimon Haars <kimon.haars@gmail.com>
2021-01-07 19:16:14 +0100

    all done, incl logic. But not made nice with scss yet. Also could add another backlink in the answer.html file

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,21 @@
 class QuestionsController < ApplicationController
 
+  def home
+  end
+
   def ask
   end
 
   def answer
+    @qu = params[:question]
+    if @qu == "I am going to work"
+      @aw = "Great!"
+    elsif @qu[-1] == "?"
+      @aw = "Silly question, get dressed and go to work!"
+    else
+      @aw = "I don't care, get dressed and go to work!"
+    end
+
   end
 
 end
